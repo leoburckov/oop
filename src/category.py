24ee6f81062da206527ec37bf0f97ce502175f44
leoburckov <burckovleo@gmail.com>
2025-08-07 20:57:33 +0400

    done

--- src/category.py
+++ src/category.py
@@ -1,34 +1,42 @@
-from typing import List, Optional
+from typing import List, Union
 
 from src.product import Product
 
 
 class Category:
-    category_count = 0
-    product_count = 0
+    category_count: int = 0
+    product_count: int = 0
 
     def __init__(
-        self, name: str, description: str, products: Optional[List[Product]] = None
+        self, name: str, description: str, products: Union[List[Product], None] = None
     ):
         self.name = name
         self.description = description
-        self.__products: List[Product] = products if products is not None else []
+        self.__products: List[Product] = []
+        if products:
+            for product in products:
+                self.add_product(product)
 
         Category.category_count += 1
-        Category.product_count += sum(product.quantity for product in self.__products)
 
     def add_product(self, product: Product) -> None:
         if not isinstance(product, Product):
             raise TypeError(
-                "Можно добавлять только объекты класса Product или его подклассов"
+                "Можно добавлять только объекты типа Product или его наследников"
             )
         self.__products.append(product)
-        Category.product_count += product.quantity
-
-    def __str__(self) -> str:
-        total_quantity = sum(product.quantity for product in self.__products)
-        return f"{self.name}, количество продуктов: {total_quantity} шт."
+        Category.product_count += 1
 
     @property
     def products(self) -> str:
-        return "\n".join(str(p) for p in self.__products)
+        return "\n".join(
+            [
+                f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт."
+                for product in self.__products
+            ]
+        )
+
+    @classmethod
+    def reset_counts(cls):
+        cls.category_count = 0
+        cls.product_count = 0
